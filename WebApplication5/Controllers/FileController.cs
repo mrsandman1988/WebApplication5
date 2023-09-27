cb7a0264d0abc9cb93d54a148cdecb50027692c5
sraphunanyan <User@ADMIN>
2023-09-27 19:52:17 +0400

    Add project files.

---
+++ WebApplication5/Controllers/FileController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly PhoneDbContext _phoneDbContext;
+        public FileController(IWebHostEnvironment webHostEnvironment, PhoneDbContext phoneDbContext)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _phoneDbContext = phoneDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Phone phone, IFormFile Image)
+        {
+
+
+            phone.FileName = "-";
+            _phoneDbContext.Phones.Add(phone);
+            _phoneDbContext.SaveChanges();
+            var filename = phone.Name.Replace(" ", "_") +  "_" + phone.Id.ToString();
+            string path = _webHostEnvironment.WebRootPath + $"/Images/{filename}";
+            using var stream = new FileStream(path, FileMode.Create);
+            Image.CopyTo(stream);
+            phone.FileName = filename;
+            _phoneDbContext.SaveChanges();
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            var phones = _phoneDbContext.Phones.ToList();
+            return View(phones);
+        }
+    }
+}
---
+++ WebApplication5/Models/Phone.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication5.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public class PhoneDbContext:DbContext
+    {
+        public DbSet<Phone> Phones { get; set;}
+
+        public PhoneDbContext(DbContextOptions<PhoneDbContext> opt) :base(opt) 
+        { }
+    }
+}
